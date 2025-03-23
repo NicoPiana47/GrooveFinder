7403befe3298a1628b2e337970d57b970ea53070
Facundo Piana Sampietro <facusampi@hotmail.com>
2025-03-23 19:42:41 -0300

    develop: Se crea header, footer, dashboard, rutas basicas, se implementa prime-icons, etc.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './groove-finder/pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'groove-finder',
+        loadComponent: () => import("./groove-finder/layouts/groove-finder-layout/groove-finder-layout.component").then((c) => c.GrooveFinderLayoutComponent),
+        children: [
+            {
+                path: 'dashboard',
+                component: DashboardComponent,
+            },
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+        ]
+    },
+    {
+        path: '**',
+        redirectTo: 'groove-finder'
+    },
+];
---
+++ src/app/groove-finder/layouts/groove-finder-layout/header/header.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-header',
+  imports: [MenubarModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  protected menuItems: MenuItem[] = [];
+
+  
+  ngOnInit() {
+    this.menuItems = [
+      {
+          label: 'Mural',
+          icon: 'pi pi-objects-column',
+          routerLink: 'groove-finder/dashboard'
+      },
+      {
+        label: 'Crear nuevo Groove',
+        icon: 'pi pi-plus',
+        routerLink: 'create',
+      },
+    ];
+  }
+}
